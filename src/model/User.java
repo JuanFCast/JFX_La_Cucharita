bdc4fed73df9b777c7f91792762745fb7f9d8d91
Juan Camilo <juancaram21@gmail.com>
2021-09-19 12:51:32 -0500

    Se realizaron los siguientes cambios:

--- src/model/User.java
+++ src/model/User.java
@@ -2,13 +2,36 @@ package model;
 
 public class User {
 	
-	String ID;
+	private String name;
+	private String id;
+	private String birthday;
+	private String password;
 	
 	
-	public User(String id) {
-		ID = id;
+	//Constructor
+	public User(String name, String id, String birthday, String password) {
+		this.name = name;
+		this.id = id;
+		this.birthday = birthday;
+		this.password = password;
 	}
+
 	
-	
+	//Getters
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,44 +4,8 @@ import java.io.IOException;
 
 public class Main {
 	
-	
-	
 	public static void main(String[] args) throws IOException {
-        launch(args);
+        
     }
-
-	private static void launch(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-
-	public static void JesusD() {
-		System.out.println("Hola mundo");
-	}
-
-
-	public void JuanK() {
-		int i = 0;
-		
-		System.out.println("Hola mis amiguitos");
-		if(i == 9) {
-			
-			
-		}
-	}
-
-	public int JuanFCast(){
-		int option;
-
-    	System.out.println("Hello World");
-        option = sc.nextInt();
-        sc.nextLine();
-
-        return option;
-    	
-	}
-	
-
 	
 }
