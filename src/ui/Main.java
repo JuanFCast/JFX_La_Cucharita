0fa12c9713d13d8e7cc8a21ea5db96386492d04c
Juan Camilo <juancaram21@gmail.com>
2021-09-19 13:00:00 -0500

    Se realizan los siguientes cambios: 1. Se configura la regla javafx en el Java Build Path,  2. Se hace la extencion de Application en el Main con sus respectivos imports y metodos obligatorios,  3. Se crea la clase User en el model,  4. Se crea el archivos employees_module.fxml en el ui

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,10 +2,19 @@ package ui;
 
 import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 	
 	public static void main(String[] args) throws IOException {
         
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
