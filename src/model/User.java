0c84ee038040592791ae81912ed9d9795c9d93dd
Juan Camilo <juancaram21@gmail.com>
2021-09-20 19:36:16 -0500

    Se realizaron los siguientes cambios: 1. Se crea la clase Restaurant (clase principal del model),  2. Se modifica la clase RestaurantGUI,  3. El programa ya permite ingresar al modulo de empleados mediante un usuario registrado

--- src/model/User.java
+++ src/model/User.java
@@ -2,18 +2,28 @@ package model;
 
 public class User {
 	
-	private String name;
 	private String id;
+	private String name;
 	private String birthday;
 	private String password;
+	private String role;
 	
 	
 	//Constructor
-	public User(String name, String id, String birthday, String password) {
+	public User(String id, String name, String birthday, String password, String role) {
 		this.name = name;
 		this.id = id;
 		this.birthday = birthday;
 		this.password = password;
+		this.role = role;
+	}
+	
+	//Constructor overloading
+	public User(String id, String name, String password, String role) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.role = role;
 	}
 
 	
@@ -32,6 +42,10 @@ public class User {
 
 	public String getPassword() {
 		return password;
+	}
+	
+	public String getRole() {
+		return role;
 	}
 	
 }
