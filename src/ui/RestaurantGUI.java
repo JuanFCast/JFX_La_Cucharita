1082b0172cff30aab3823b61479a511c257ee396
JUAN FELIPE CASTILLO GOMEZ <juanfelipe.castillogomez@gmail.com>
2021-09-23 17:41:17 -0500

    Se agrega el metodo de agregar empleado

--- src/ui/RestaurantGUI.java
+++ src/ui/RestaurantGUI.java
@@ -5,14 +5,20 @@ import java.io.IOException;
 import javax.swing.JOptionPane;
 
 import javafx.fxml.FXMLLoader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,8 +27,56 @@ import model.Inventory;
 import model.MEASUREMENT_TYPE;
 import model.Restaurant;
 
+
 //Clase controladora, aqui poner todo lo referente a lo grafico (Es como el Main en nuestros proyectos)
 public class RestaurantGUI {
+	
+	///////////////////////////////////////////////////////////list-employees
+	@FXML
+    private TableView<?> tableAccList;
+
+    @FXML
+    private TableColumn<?, ?> colUserName;
+
+    @FXML
+    private TableColumn<?, ?> ID;
+
+    @FXML
+    private TableColumn<?, ?> colBirthday;
+    
+
+   
+    @FXML
+    private TextField txtUserName;
+    
+    @FXML
+    private TextField id;
+    
+    @FXML
+    private DatePicker birthday;
+    
+    
+    @FXML
+    private PasswordField passwordField;
+    
+
+   
+    ///////////////////////////////////////////////////////////list-employees
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//Variables
 	@FXML
@@ -154,7 +208,7 @@ public class RestaurantGUI {
 	}
 	
 	
-	
+	///////////////////////////////////////////////////////////list-employees
 	//Este metodo muestra la pantalla del modulo de empleados
 	public void OpenEmployees() throws IOException {
 		FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("list-employe.fxml"));
@@ -164,6 +218,69 @@ public class RestaurantGUI {
 
 		
 	}
+	
+	
+	
+	//Este metodo hace el registor a un empleado
+    @FXML
+    public void createAccount(ActionEvent event) {
+    	
+		
+    	
+    	if(!txtUserName.getText().equals("") && !id.getText().equals("") &&birthday.getValue()!=null  &&  !passwordField.getText().equals("")){
+	    	
+    		
+    		
+    		classroom.createAccount(txtUserName.getText(), passwordField.getText(),txtProfilePhoto.getText(),gend, career,birthday.getValue(), browser.getSelectionModel().getSelectedItem());
+    		
+    		Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setTitle("Cuenta creada");
+    		alert.setHeaderText(null);
+    		alert.setContentText("Se ha creado un nuevo empleado!" + "\n" + "Bienvenido " + txtUserName.getText() + "!");
+
+    		alert.showAndWait();
+    		
+    		txtUserName.clear();
+    		id.clear();
+        	passwordField.clear();
+        	
+        	birthday.setValue(null);
+        	
+        	
+    
+        	
+    	}else {
+    		Alert alert = new Alert(AlertType.ERROR);
+    		alert.setTitle("Acceso denegado");
+    		alert.setHeaderText(null);
+    		alert.setContentText("Debes completar cada campo en el formulario");
+
+    		alert.showAndWait();
+    	}
+
+    }
+	
+	
+	
+	public void initializeTableView() {
+    	ObservableList<UserAccount> list= FXCollections.observableArrayList(classroom.getAccounts());
+    	
+    	tableAccList.setItems(list);
+    	colUserName.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("userName"));
+    	colGender.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("gender"));
+    	colCareer.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("career"));
+    	colBirthday.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("birthday"));
+    	colBrowser.setCellValueFactory(new PropertyValueFactory<UserAccount,String>("browser"));
+	
+    	
+	}
+	
+	
+	///////////////////////////////////////////////////////////list-employees
+	
+	
+	
+	
 	
 	
 	
